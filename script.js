b16c75e873a9487eceb62cc1c9ad7552bf4cb41b
Russell James <russelljames202@gmail.com>
2022-09-16 19:49:23 -0400

    Refactor existing functions and add resetDisplay()
    Refactored appendExpression() to check for solvable expressions if
    there is a trailing operator in expression (ex. 4++ or 5+4-). This is
    so only one expression will be solved at a time.
    
    Refactored evaluateExpression() to ignore the first operator in
    expression is there are multiple, so negative numbers won’t break the
    calculator. Also to add event listener to numbers buttons so
    clicking one after expression is evaluated resets display and current
    expression stored.
    
    Refactored populateDisplay() so it detects if there is an unfinished
    expression.
    
    Added resetDisplay() as a callback function, it resets display if a
    number button is clicked after expression is evaluated with equals key
    ONLY if there isn’t an active operator.

--- script.js
+++ script.js
@@ -70,6 +70,7 @@ function multiply(x, y){
  * @returns Result of expression
  */
 function divide(x, y){
+    if (x == 0 || y == 0){return "Can't / By 0!"}
     return +x / +y;
 }
 
@@ -118,14 +119,18 @@ function addOperationListeners(){
  */
 function populateDisplay(){
     const display = document.querySelector('.display');
+    let expression = document.querySelector('.expression').innerText;
+    let operators= ['+', '-', '*', '/'];
 
-    //If display has placeholder value, add number clicked to display and expression
-    if(display.innerText === '0'|| isOperatorActive()){
+    //If display has placeholder value, operator button was clicked and there is an unfinished expression
+    if(display.innerText === '0' || (isOperatorActive() && operators.includes(expression.slice(-1)))){
         display.innerText = this.innerText;
+    
     //No more than 10 characters in display at once
-    } else if (display.innerText.length < 10){
+    }else if (display.innerText.length < 10){
         display.innerText += this.innerText;
-    }
+    } 
+
   
 }
 
@@ -138,7 +143,7 @@ function clearDisplay(){
 
     //Clear existing expression too
     const expression = document.querySelector('.expression');
-    expression.innerText = '';
+    expression.innerText = 0;
 
     //Deactivate current operator
     if(isOperatorActive()){
@@ -152,49 +157,105 @@ function clearDisplay(){
  * Stores the expression on webpage
  */
  function appendExpression(){
-    const expression = document.querySelector('.expression');
-
+    let expression = document.querySelector('.expression');
+    const display = document.querySelector('.display');
+    
+    let operators = ['+', '-', '*', '/'];
+    let trailingOperator;
+    let result;
+    
+    //Appends most recent user input to expression
     expression.innerText += this.innerText;
-}
+
+    
+    //Tests if there is an expression that can be solved before most recent operator
+   outer: for(let i = 0; i < operators.length ; i++){
+        let operator = operators[i];
+        
+        //If there is an operator in the expression and there is a trailing operator
+        if(expression.innerText.includes(operator) && 
+           operators.includes(expression.innerText[expression.innerText.length -1])){
+           
+           trailingOperator = expression.innerText.slice(-1); //get trailing operator
+           
+           //If there is no trailing operator, break loop
+           if(!trailingOperator) break outer;
+           
+           console.log('here');
+           let testExpression = expression.innerText.slice(0, -1); //get expression before
+
+            let operands = testExpression.split(operator);
+            
+            //If there is a left and right operand
+            if(operands.length == 2 && operands[1] != '' && operands[0] != ''){  
+                result = operate(operands[0], operands[1], operator);
+                break outer;
+            
+            //Put this into helper function
+            //If there is a number followed by two trailing operators (ex. 4++)
+            } else if((operators.includes(expression.innerText[expression.innerText.length -1]) &&
+                        operators.includes(expression.innerText[expression.innerText.length - 2]))){
+                let number;          
+                //Search expression for first occurrence of operator
+                for(let n = 0; n < expression.innerText.length; n++){       
+                    //Get number and use expression found
+                    if(operators.includes(expression.innerText[n])){
+                         number = expression.innerText.split(expression.innerText[n], 1);
+                         //Use the number as both left and right operand and perform operation  
+                         result = operate(number, number, expression.innerText[n]);  
+                        break outer;   
+                    }
+                }               
+            }
+        }
+    }
+
+   //If there was an expression to be evaluated...
+   if(result != undefined){
+        display.innerText = result; //display the result
+        expression.innerText = result + trailingOperator; //append the trailing operator to expression
+   }
+
+ }
 
 /**
+ * Callback function
+ * 
  * Evaluates the current expression
  */
 function evaluateExpression(){
     const display = document.querySelector('.display');
     const expression = document.querySelector('.expression');
-    
+    let operators= ['+', '-', '*', '/'];
+
     let operands; //Store left and right operands
     let result; //Store result of expression
 
     //Test for the operator inside of expression
-    if(expression.innerText.includes('+')){
+    for(let i = 0; i < expression.innerText.length; i++){
+        //Finds the first occurrence of operator in expression
+        if(operators.includes(expression.innerText[i])){
+            operands = expression.innerText.split(expression.innerText[i]);
+            result = operate(operands[0], operands[1], expression.innerText[i]);
+        } 
         
-        operands = expression.innerText.split('+');
-        result = operate(operands[0], operands[1], '+');
-        
-    } else if(expression.innerText.includes('-')){
-        
-        operands = expression.innerText.split('-');
-        result = operate(operands[0], operands[1], '-');
+    }
     
-    }else if(expression.innerText.includes('*')){
-        
-        operands = expression.innerText.split('*');
-        result = operate(operands[0], operands[1], '*');
-    
-    } else if(expression.innerText.includes('/')){
-
-        operands = expression.innerText.split('/');
-        result = operate(operands[0], operands[1], '/');
-
+    //If an invalid expression is entered don't change display 
+    if(result === undefined && result != 0){
+        result = display.innerText;
     }
 
     //Add expression to display and expression string
     display.innerText = result;
+
     expression.innerText = result;
 
     deactivateOperator();
+
+    //When a number button is clicked after evaluating, reset display
+    const numbers = document.querySelectorAll('.numbers button');
+    numbers.forEach((number) => number.addEventListener('click', resetDisplay));
 }
 
 /**
@@ -248,7 +309,30 @@ function deactivateOperator(){
     });
 }
 
+/**
+ * Callback function
+ * 
+ * Click a button resets display and expression 
+ * stored after equals button is click
+ */
+function resetDisplay(){
+    //Get numbers buttons reference
+    const display = document.querySelector('.display');
+    const expression = document.querySelector('.expression');
+    const numbers = document.querySelectorAll('.numbers button');
 
-//Make function to get nodelist/node of selector?
+    //If user wants to perform another operations, don't reset display
+    if(!isOperatorActive()){
+        display.innerText = this.innerText;
+        expression.innerText = this.innerText;
+
+        //After event is fired, remove the event listeners
+        numbers.forEach((number) => number.removeEventListener('click', resetDisplay));
+    }
+}
+
+//Format long decimal numbers
+//If number gets too long, display NaN
+//Try to break the calculator some more
 
 addButtonListeners();
